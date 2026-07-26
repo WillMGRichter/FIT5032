30f282ba6401e3bf66128b4d3be0fec1cb283db7
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2026-07-26 11:51:59 +1000

    Removing Unrequired code from myName

--- fit5032-week1/src/components/MyName.vue
+++ fit5032-week1/src/components/MyName.vue
@@ -1,12 +1,3 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
-</script>
-
 <template>
   <div class="greetings">
     <h1 class="green">William Richter</h1>
