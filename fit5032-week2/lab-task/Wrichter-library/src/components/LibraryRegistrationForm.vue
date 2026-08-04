b7bea480d60c53356e09cc4d080d56d2a51b481d
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2026-08-04 17:16:55 +1000

    Lab 3:  lab setup

---
+++ fit5032-week2/lab-task/Wrichter-library/src/components/LibraryRegistrationForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>User Information Form / Credentials</h1>
+    </div>
+</template>
+
+<script setup>
+// Our logic will go here
+</script>
+
+ // Our logic will go here
+<style scoped>
+   
+</style>
