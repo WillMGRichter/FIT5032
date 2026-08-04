50a4f66892ba1cc7c08f01caa401f2e8e3742f36
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2026-08-04 17:29:40 +1000

    Lab 3: Implementation of task 6

--- fit5032-week2/lab-task/Wrichter-library/src/components/LibraryRegistrationForm.vue
+++ fit5032-week2/lab-task/Wrichter-library/src/components/LibraryRegistrationForm.vue
@@ -83,14 +83,69 @@
       </div>
     </div>
   </div>
+
+  <div class="row mt-5" v-if="submittedCards.length">
+   <div class="d-flex flex-wrap justify-content-start">
+      <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
+         <div class="card-header">
+            User Information
+         </div>
+         <ul class="list-group list-group-flush">
+            <li class="list-group-item">Username: {{ card.username }}</li>
+            <li class="list-group-item">Password: {{ card.password }}</li>
+            <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
+            <li class="list-group-item">Gender: {{ card.gender }}</li>
+            <li class="list-group-item">Reason: {{ card.reason }}</li>
+         </ul>
+      </div>
+   </div>
+</div>
 </template>
 
 
 <script setup>
-// Our logic will go here
+    import { ref } from 'vue';
+    
+    const formData = ref({
+        username: '',
+        password: '',
+        isAustralian: false,
+        reason: '',
+        gender: ''
+    });
+
+    const submittedCards = ref([]);
+
+    const submitForm = () => {
+        submittedCards.value.push({
+            ...formData.value
+        });
+    };
+
+    const clearForm = () => {
+        formData.value = {
+            username: '',
+            password: '',
+            isAustralian: false,
+            reason: '',
+            gender: ''
+        };
+    };
 </script>
 
- // Our logic will go here
 <style scoped>
-   
+   .card {
+   border: 1px solid #ccc;
+   border-radius: 10px;
+   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+   }
+   .card-header {
+   background-color: #275FDA;
+   color: white;
+   padding: 10px;
+   border-radius: 10px 10px 0 0;
+   }
+   .list-group-item {
+   padding: 10px;
+   }
 </style>
