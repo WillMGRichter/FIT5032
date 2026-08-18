e5e6a214f2f8e82f9f6032c3ac452089ac7850c7
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2026-08-18 14:06:10 +1000

    Lab 4: Fixing dependencies and main.js

--- fit5032-week4/lab-task/Wrichter-library/src/main.js
+++ fit5032-week4/lab-task/Wrichter-library/src/main.js
@@ -1,6 +1,21 @@
-//import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        present: Aura
+    }
+})
+
+app.mount('#app')
