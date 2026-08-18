e2d78ea1c32869acdefa2854a99c9d59814d595a
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2026-08-18 14:25:48 +1000

    lab 5: Moving Given nav bar to app.vue

--- fit5032-week5/lab-task/Wrichter-library/src/App.vue
+++ fit5032-week5/lab-task/Wrichter-library/src/App.vue
@@ -4,6 +4,17 @@ import Form from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
+  <div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a href="#" class="nav-link active" aria-current="page">Home (Week 5)</a>
+        </li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact us</a></li>
+      </ul>
+    </header>
+  </div>
   <JSON />
   <Form />
 </template>
