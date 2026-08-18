785859b07ca85322b8dacaa9cbd19ab793d6dac8
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2026-08-18 15:05:07 +1000

    Lab 5 T5: Moving and renaming files

--- fit5032-week5/lab-task/Wrichter-library/src/App.vue
+++ fit5032-week5/lab-task/Wrichter-library/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import JSON from './components/JSON.vue'
-import Form from './components/LibraryRegistrationForm.vue'
+import Form from './views/HomeView.vue'
+
 </script>
 
 <template>
