805b56270b00833f52dba2b04591124dacb9db53
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2026-08-18 14:22:08 +1000

    Seeding Lab 5

---
+++ fit5032-week5/lab-task/Wrichter-library/src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        present: Aura
+    }
+})
+
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
+
+app.mount('#app')
