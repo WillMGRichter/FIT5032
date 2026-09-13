8f048eb2e189d4ea64499c9df1b05a26acb39eb6
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2026-09-13 14:47:52 +1000

    Project: Fixing development bug

--- assignment/vite.config.js
+++ assignment/vite.config.js
@@ -2,17 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(async ({ command }) => {
+  const plugins = [vue()]
+  if (command === 'serve') {
+    const { default: vueDevTools } = await import('vite-plugin-vue-devtools')
+    plugins.push(vueDevTools())
+  }
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
